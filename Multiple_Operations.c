0763fbce87295447c4880dac5bc3de182dd64836
hrishikeshp7 <68657160+hrishikeshp7@users.noreply.github.com>
2020-12-10 14:33:34 +0530

    Create Multiple_Operations.c

---
+++ Multiple_Operations.c
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main () {
+    
+    float a,b,c,d,e,f;
+       cout<< "Enter Two Integers";
+        cin >>a>> b;
+        
+        c=a+b;
+        d=b-a;
+        e=a*b;
+        f=a/b;
+       
+    cout<< "Addition of a and b is ";
+    cout<<c << endl;
+    cout<<" Subtraction of a and b is";
+    cout<<d << endl;
+    cout<< " Multiplication of a and b is";
+    cout<<e << endl;
+    cout<< "division of a and b is:";
+    cout<<f <<endl;
+    
+    return 0;
+    
+}
