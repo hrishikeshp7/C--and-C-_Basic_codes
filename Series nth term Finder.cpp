d35a8f06bd0776c502b68e5dc0d1c153889082a1
hrishikeshp7 <68657160+hrishikeshp7@users.noreply.github.com>
2021-06-15 17:04:07 +0530

    Update Series nth term Finder.cpp

--- Series nth term Finder.cpp
+++ Series nth term Finder.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    float a ;
+    cout << "Enter which number of term you would like to find " <<endl;
+    cin >> a;
+    float b ;
+    cout << "Enter the first Number of The Series (Should Be in AP)" <<endl;
+    cin >> b;
+    float c;
+    cout << "Enter the Common Difference between the numbers" <<endl;
+    cin >> c;
+
+    float n;
+    n =   b + (a-1)*c ;
+
+    cout << "The Nth Term of The AP is " <<n << endl;
+
+    return 0 ;
+
+}
