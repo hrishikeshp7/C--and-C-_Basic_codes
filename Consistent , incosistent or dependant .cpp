9e29ac12dbe0d27c6383fd75ec7581b6b5ac418f
hrishikeshp7 <68657160+hrishikeshp7@users.noreply.github.com>
2021-06-16 08:57:52 +0530

    Create Consistent , incosistent or dependant .cpp

---
+++ Consistent , incosistent or dependant .cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <math>
+
+using namespace std;
+// Program to find if The Equation is Consistent , Incosistent or Dependant
+
+int main () {
+    float a , b, c , d , e , f , g, h , i , j , k , l , m ;
+    cout << "Enter the Coefficents of a1 , b1 , c1 Respectively " << endl;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cout << "Enter the Coefficents of a2 , b2 , c2 Respectively " << endl;
+    cin >> d;
+    cin >> e;
+    cin >> f;
+
+    g = a/d ;
+    h = b/e ;
+    i = c/f ;
+
+    if (g = h = i ){
+        cout << "The Two Equations are Dependant " << endl;
+
+    else {
+        cout << "Moving to Next step " << endl;
+
+    }
+    if (g = h )
+
+    }
+
+}
