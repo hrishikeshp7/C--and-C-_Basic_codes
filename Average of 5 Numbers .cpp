cb2c507004e5e150c24fd33af170ff7ca48937f3
hrishikeshp7 <68657160+hrishikeshp7@users.noreply.github.com>
2021-06-16 13:38:15 +0530

    Create Average of 5 Numbers .cpp

---
+++ Average of 5 Numbers .cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+// C++ Program To find Average of 5 Numbers (You can modify , Its an Example)
+int main () {
+    float a , b , c, d , e , f ;
+    cout << "Enter the Five Numbers In Sequence " << endl;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e ;
+
+    f = (a + b + c + d + e)/5 ;
+
+    cout << "The Average of Five Numbers is " << f ;
+
+    return 0;
+
+}
